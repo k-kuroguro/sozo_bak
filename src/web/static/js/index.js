9996e20ee5ab4469537f41b85b7803f3aa0c1735
k-kuroguro <lingjiujinshan@gmail.com>
2024-11-08 19:13:36 +0900

    a

--- src/web/static/js/index.js
+++ src/web/static/js/index.js
@@ -1,46 +1,15 @@
-let reconnectFrequencySeconds = 2;
-let reconnectInterval;
-
 const createEventSource = () => {
-   let es = new EventSource('/monitor');
+   const es = new EventSource('/monitor');
 
    es.addEventListener('message', function (event) {
       console.log(event.data);
    });
    es.addEventListener('error', function (event) {
       console.log('Error', event);
-      es.close();
    });
-   es.addEventListener('open', function (event) {
-      reconnectFrequencySeconds = 2;
-      if (reconnectInterval) {
-         clearInterval(reconnectInterval);
-      }
-   });
-   window.addEventListener('beforeunload', function () {
-      es.close();
-      attemptReconnect();
-   });
+   es.addEventListener('open', function (event) { });
 
    return es;
 };
 
-
-const attemptReconnect = () => {
-   if (es.readyState === 2) {
-      es = createEventSource();
-      reconnectFrequencySeconds = Math.min(reconnectFrequencySeconds * 2, 64); 
-      console.log(reconnectFrequencySeconds);
-      restartReconnectInterval();
-   }
-};
-
-const restartReconnectInterval = () => {
-   if (reconnectInterval) {
-      clearInterval(reconnectInterval);
-   }
-   reconnectInterval = setInterval(attemptReconnect, reconnectFrequencySeconds * 1000);
-};
-
 es = createEventSource();
-attemptReconnect();
