5f6baae8b2ee9fbe038ffe5c49ff812b66c4cd1d
k-kuroguro <lingjiujinshan@gmail.com>
2024-11-09 11:54:19 +0900

    wip

--- src/web/static/js/index.js
+++ src/web/static/js/index.js
@@ -3,9 +3,11 @@ const createEventSource = () => {
 
    es.addEventListener('status_msg', function (event) {
       console.log(event.data);
+      document.getElementById('score').innerHTML = `Status: ${event.data}`;
    });
    es.addEventListener('error_msg', function (event) {
       console.log(event.data);
+      document.getElementById('score').innerHTML = `Error: ${event.data}`;
    });
    es.addEventListener('error', function (event) {
       console.log('Error', event);
